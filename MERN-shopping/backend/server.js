2179d8a91bd3585f18e748854ba98c73d66edac4
Hakan Durmaz <hakandurmz@hotmail.com>
2021-01-22 00:07:38 +0300

    connected mongodb atlas
    creacted user, order, product model
    created seeder.js for import or destroy data

---
+++ MERN-shopping/backend/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import products from './data/products.js';
+import connectDB from './config/db.js';
+import dotenv from 'dotenv';
+import colors from 'colors';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('api start..');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
