0cf5114ee56acc30c1958c1ed1cb6dbe245ddd92
Hakan Durmaz <hakandurmz@hotmail.com>
2021-01-22 16:25:32 +0300

    Added middware error handler, productRoutes updated
    Added middleware error handler
    productRoutes updated

--- MERN-shopping/backend/server.js
+++ MERN-shopping/backend/server.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import products from './data/products.js';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
 
@@ -11,17 +12,13 @@ connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('api start..');
+  res.send('api started..');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
