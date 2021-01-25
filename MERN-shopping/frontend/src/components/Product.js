421b3debc71371da84c22404779e4169cfc69adb
Hakan Durmaz <hakandurmz@hotmail.com>
2021-01-25 14:34:46 +0300

    Importing React Redux
    Imported useDispatch and useSelector Redux
    Added Loader component and Message component
    Created Store.js

--- MERN-shopping/frontend/src/components/Product.js
+++ MERN-shopping/frontend/src/components/Product.js
@@ -4,12 +4,10 @@ import Rating from './Rating';
 import { Link } from 'react-router-dom';
 
 const Product = ({ product }) => {
-  console.log(product);
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
-        {console.log(product)}
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
